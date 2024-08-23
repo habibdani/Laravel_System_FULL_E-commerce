450fc0b416af577899348bc16615870255a5bafc
habibdani <habibdani86@gmail.com>
2024-08-23 17:55:27 +0700

    clear all API backend

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ShippingController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
-
 
 // Rute untuk menampilkan form login
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
@@ -13,21 +10,9 @@ Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-
-
-
-
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
-//     // Tambahkan route lainnya yang ingin diproteksi di sini
-// });
+Route::get('/about', function () {
+    return view('about');
+});
