6b5970fceb663884ff87ada55164fe664149a313
Hanif Ramadhan <h4nif.ramadhan@gmail.com>
2024-11-29 02:03:19 +0700

    change: change vite config

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        https: true, // Aktifkan HTTPS
-        host: '0.0.0.0', // Terima semua permintaan, termasuk dari subdomain
-        hmr: {
-            host: 'andalprima.hansmade.online', // Host subdomain Anda
-            protocol: 'wss', // Gunakan WebSocket Secure
-        },
-    },
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/maps-script.js'
             ],
             refresh: true,
         }),
     ],
-    publicDir: 'public', // Menambahkan rule agar folder fonts termasuk dalam build
+    build: {
+        outDir: 'public/build', // Direktori output untuk build production
+        assetsDir: '',          // Letakkan file di root `public/build`
+    },
+    publicDir: 'public', // Folder untuk static assets
 });
